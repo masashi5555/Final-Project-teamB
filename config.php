32d675b32c576e5696ab5a4e24dde6921f524853
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-12-24 16:49:38 -0800

    modify the conflict and change the basename

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php
 
-    $hostName = "localhost";
-    $userName = "root";
-    $password = "";
-    $dbName = "app_db";
-    $baseName = "http://localhost/PHP/part1/Calendar/";
-    // session_start();
+
+$hostName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "app_db";
+$baseName = "http://localhost/PHP/part1/Calendar/";
+// session_start();
+
+
+//mauricio's basename and dbconfig.
+// $baseName = "http://localhost/php/Final-Project-teamB/";
+$dbConfig = ['localhost','root','','crm'];
 ?>
--- sellsPage.php
+++ sellsPage.php
@@ -7,6 +7,12 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
 
+    <style>
+        .grpah {
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
